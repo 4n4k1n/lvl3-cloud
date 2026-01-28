72b733ce91f5451c806f57b9d035764bc5706de6
Anakin Pregitzer <anakin@pregitzer.de>
2026-01-28 12:55:56 +0000

    check first if terraform init was already executed

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -6,7 +6,11 @@ source /opt/stack/devstack/openrc admin admin
 
 # Initialize and apply terraform
 cd /opt/stack/lvl3-cloud/infra/terraform/k8s
-terraform init
+
+if [ ! -d .terraform ]; then
+    terraform init
+fi
+
 terraform apply -auto-approve
 
 cd /opt/stack/lvl3-cloud/infra/ansible && ansible-playbook -i inventory.ini site.yml
